f5a97d9d9e9dd637702071b71da2e42dcecf0168
xurongcai <rongcai.xu@ivydad.com>
2022-06-29 08:31:16 +0800

    add logstorage demo

--- examples/logstorage/logstorage.go
+++ examples/logstorage/logstorage.go
@@ -52,7 +52,7 @@ func main() {
 	meta := make(map[string]string)
 
 	logRequest := pb.LogstorageMessageRequest{
-		LogstorageName: "alicloud",
+		LogstorageName: "alicloud.slsLogstorage",
 		Project:        "k8s-log-c134d574cd3e6405682e2dda2095ea35d",
 		Logstore:       "person-log",
 		Topic:          "dapr-demo",
